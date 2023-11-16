d013cc3d69dd6408c01ab1bc4dbc53a243fea94b
Alfred <fredblade142@gmail.com>
2023-11-15 19:30:20 -0500

    dsadsada

--- src/App.vue
+++ src/App.vue
@@ -12,22 +12,29 @@ function coinflip() {
 }
 
 </script>
-
 <template>
-  <div>
-    <div>
+  <div class="container">
+    <div class="item">
       <h1 class="title is-1">Heads or tails</h1>
       <br>
       <button class="button" @click="coinflip">Click me!</button>
       <br>
       <br>
-      <img v-if="coin.value === 0" class="image is-128x128" src="/src/assets/tails.png">
-      <img v-if="coin.value === 1" class="image is-128x128" src="/src/assets/heads.png">
-      <img v-if="coin.value === 3" class="image" src="/src/assets/coin.jpg">
+      <img v-if="coin.value === 0" src="/src/assets/tails.png">
+      <img v-if="coin.value === 1" src="/src/assets/heads.png">
+      <img v-if="coin.value === 3" src="/src/assets/coin.png">
     </div>
-
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.item {
+  max-width: 50%;
+}
 </style>
